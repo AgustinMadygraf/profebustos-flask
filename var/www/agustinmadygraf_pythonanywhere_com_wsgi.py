b083af6b4cb4904ad8f18001a2b69dfff9b6f0d0
AgustinMadygraf <agustin.mtto.madygraf>
2025-11-02 20:32:17 -0300

    Remove MySQL status endpoint and update WSGI file to load environment variables

--- var/www/agustinmadygraf_pythonanywhere_com_wsgi.py
+++ var/www/agustinmadygraf_pythonanywhere_com_wsgi.py
@@ -3,10 +3,14 @@ Path: var/www/agustinmadygraf_pythonanywhere_com_wsgi.py
 """
 
 import sys
+import os
+from dotenv import load_dotenv
 
 PROJECT_ROOT = '/home/agustinmadygraf/profebustos-flask'
 if PROJECT_ROOT not in sys.path:
     sys.path.insert(0, PROJECT_ROOT)
 
-# importa tu app con ruta absoluta de paquete
+# Carga el archivo .env antes de importar la app
+load_dotenv(os.path.join(PROJECT_ROOT, '.env'))
+
 from src.infrastructure.flask.flask_app import app as application
